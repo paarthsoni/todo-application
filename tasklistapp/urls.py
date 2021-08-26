c2c9bdd8bd786d7133a774f1d88aff3d225a1e40
paarthsoni <paarth2812@gmail.com>
2021-08-26 16:27:26 +0530

    fifth commit

--- tasklistapp/urls.py
+++ tasklistapp/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.views.static import serve
+from django.conf.urls import url
+
+from django.conf import settings
+
 admin.site.site_header="Todo App admin"
 admin.site.site_header="Todo App  admin portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
